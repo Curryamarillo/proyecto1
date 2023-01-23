a9814798d88ea27c28b4c511eb3a6f46cb6dcb10
Gustavo Starace <practicodecocina@gmail.com>
2023-01-23 15:41:55 +0100

    segundo commit

--- src/DatosImprimir/Impresor.java
+++ src/DatosImprimir/Impresor.java
@@ -1,2 +1,29 @@
-package DatosImprimir;public class Impresor {
+package DatosImprimir;
+
+
+public class Impresor {
+    public static void main(String[] args) {
+        byte num = 5;
+        short num1 = 12;
+        int num2 = 25;
+        long num3 = 25422227;
+        float num4 = 25.5F;
+        double num5 = 25.5544d;
+        char char1 = 'a';
+        String cadena = "Jauja";
+
+        System.out.println(num);
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(char1);
+        System.out.println(cadena);
+
+
+
+
+
+    }
 }
